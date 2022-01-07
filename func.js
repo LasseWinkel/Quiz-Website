c06729e33118644641a67c69673293368b3e697f
Lasse Winkel <lasse-winkel@t-online.de>
2022-01-07 13:43:52 +0100

    Kontaktformular wird hinzugefügt

--- func.js
+++ func.js
@@ -139,3 +139,14 @@ function clearAll() {
 document.addEventListener("DOMContentLoaded", function (event) {
   getScore();
 });
+
+// Email-Validation
+function ValidateEmail(inputText) {
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (inputText.value.match(mailformat)) {
+    return true;
+  } else {
+    alert("You have entered an invalid email address!");
+    return false;
+  }
+}
